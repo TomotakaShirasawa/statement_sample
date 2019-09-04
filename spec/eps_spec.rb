abd2ca532a7f93a7e7a18698e62ebd306d283e2a
Shirasawa <shirasawa@minkabu.co.jp>
2019-09-04 20:29:38 +0900

    :recycle: (EPS): 説明しやすくするためにPLを無くした

--- spec/eps_spec.rb
+++ spec/eps_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 RSpec.describe 'EPS' do
   before do
     # Softbankの例
-    @pl = PL.new(net_income: NetIncome.new(1_411_199))
+    @ni = NetIncome.new(1_411_199)
     @os = OutstandingShares.new(1_100_660_365)
   end
 
   it('NetIncomeがEPSを返す') do
-    eps = @pl.net_income.eps(@os)
+    eps = @ni.eps(@os)
     expect(eps.to_d.to_f).to be_within(0.01).of(1282.13) # to_fの方がチェックしやすい
   end
 
   it('OutstandingSharesがEPSを返す') do
-    eps = @os.eps(@pl.net_income)
+    eps = @os.eps(@ni)
     expect(eps.to_d.to_f).to be_within(0.01).of(1282.13) # to_fの方がチェックしやすい
   end
 end
