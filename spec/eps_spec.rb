3ac2e81d2f30bac36c573fbeddb0691bb049d1fa
Shirasawa <shirasawa@minkabu.co.jp>
2019-10-17 22:28:49 +0900

    (PER): 1ケースしか無いので簡素化

--- spec/eps_spec.rb
+++ spec/eps_spec.rb
@@ -2,14 +2,11 @@ require 'spec_helper'
 
 RSpec.describe EPS do
   describe 'PERを計算' do
-    before do
-      eps = described_class.new(eps_value)
-      stock_price = StockPrice.new(stock_price_value)
-      @per = eps.per(stock_price)
+    it do
+      eps = described_class.new(23.11)
+      stock_price = StockPrice.new(2620.0)
+      per = eps.per(stock_price)
+      expect(per.to_d).to be_within(0.01).of(113.37)
     end
-
-    let(:eps_value) { 23.11 }
-    let(:stock_price_value) { 2620.0 }
-    it { expect(@per.to_d).to be_within(0.01).of(113.37) }
   end
 end
